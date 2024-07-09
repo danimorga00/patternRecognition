f1ec5f93b874b280df88c4075d698f5e4f18747a
danimorgan00 <daniele.morganti07@gmail.com>
2024-07-09 17:44:26 +0200

    Aggiunto SAD

--- searchTimeSeries.cpp
+++ searchTimeSeries.cpp
@@ -1,6 +1,38 @@
 #include "searchTimeSeries.hpp"
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <limits>
 #include <omp.h>
 
+double calculateSAD(const std::vector<double>& series1, const std::vector<double>& series2, int startIdx) {
+    double sad = 0.0;
+    int size = series2.size();
+    for (int i = 0; i < size; ++i) {
+        sad += std::abs(series1[startIdx + i] - series2[i]);
+    }
+    return sad;
+}
+
+int findBestMatch(const std::vector<double>& longSeries, const std::vector<double>& shortSeries) {
+    int longSize = longSeries.size();
+    int shortSize = shortSeries.size();
+    double minSAD = std::numeric_limits<double>::max();
+    int bestMatchIdx = -1;
+
+    #pragma omp parallel for
+    for (int i = 0; i <= longSize - shortSize; ++i) {
+        double sad = calculateSAD(longSeries, shortSeries, i);
+        if (sad < minSAD) {
+            #pragma omp critical
+            {
+                minSAD = sad;
+                bestMatchIdx = i;
+            }
+        }
+    }
+    return bestMatchIdx;
+}
 // Definizione della funzione per cercare la serie temporale corta nella serie temporale lunga
 std::vector<int> searchTimeSeries(const std::vector<int>& longSeries, const std::vector<int>& shortSeries) {
     std::vector<int> results;
--- searchTimeSeries.hpp
+++ searchTimeSeries.hpp
@@ -9,5 +9,8 @@
 // Dichiarazione della funzione per cercare la serie temporale corta nella serie temporale lunga
 std::vector<int> searchTimeSeries(const std::vector<int>& longSeries, const std::vector<int>& shortSeries);
 std::vector<int> extractRandomSubarray(const std::vector<int>& array, int n);
+int findBestMatch(const std::vector<double>& longSeries, const std::vector<double>& shortSeries);
+double calculateSAD(const std::vector<double>& series1, const std::vector<double>& series2, int startIdx);
+
 
 #endif // TIMESERIESSEARCH_H
